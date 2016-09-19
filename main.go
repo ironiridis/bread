679c40429fccf597598fd9fa5dbfc57c25d0f444
C. Harrington <ironiridis@gmail.com>
2016-09-19 17:10:01 -0500

    etc

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+// This is going to be a very poorly factored golang app. MVP and all that, you know.
+
+import "log"
+import "time"
+import "github.com/ironiridis/tension"
+import "github.com/ironiridis/private"
+
+func main() {
+	s := tension.New(private.SlackTestToken())
+	tx, rx, err := slackRTMWebsocket(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("connected, presumably")
+
+	for {
+		select {
+		case rxmsg := <-rx:
+			log.Printf("%s", rxmsg)
+		case <-time.After(time.Second * 10):
+			tx <- `{"id": 1, "type": "ping"}`
+		}
+	}
+}
